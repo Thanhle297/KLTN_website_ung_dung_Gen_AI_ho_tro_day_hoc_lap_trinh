48e39e6ee02418a5d8cb7ec3d1235d7fd0e58aec
Thanhle297 <stu725102086@hnue.edu.vn>
2026-02-10 16:53:43 +0700

    bỏ log

--- frontend/src/components/QuestionPanel.jsx
+++ frontend/src/components/QuestionPanel.jsx
@@ -1,10 +1,6 @@
 import "../styles/QuestionPanel.scss";
-import { useEffect } from "react";
 
 export default function QuestionPanel({ current }) {
-  // useEffect(() => {
-  //   console.log("QUESTION HTML:", current.question);
-  // }, [current]);
 
   return (
     <div className="question-panel">
